08640f59facc948bbf85fd749ddfcacf17e7c12d
Felipe FCRL <145389507+Lipe7k@users.noreply.github.com>
2025-02-02 13:40:45 -0300

    adicionando modo dark

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+const trilho = document.querySelector(".trilho")
+const body = document.querySelector("body")
+
+trilho.addEventListener('click', () => {
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
